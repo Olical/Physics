8a8d7b8471681910345ce9eeb49483ff095070fe
Oliver Caldwell <oliver@flowdev.co.uk>
2011-07-17 21:35:54 +0100

    Added bare bones for world

--- Physics.js
+++ Physics.js
@@ -5,7 +5,31 @@
  */
 var Physics = {
 	World: new Class({
-		
+		Implements: [Events, Options],
+		options: {
+			fps: 12,
+			width: 500,
+			height: 500
+		},
+		initialize: function(options) {
+			// Set the options
+			this.setOptions(options);
+			
+			// Set up the particle array
+			this.particles = [];
+			
+			// Start the loop
+			setInterval(this.step, 1000 / fps);
+		},
+		step: function() {
+			
+		},
+		addParticle: function() {
+			
+		},
+		removeParticle: function() {
+			
+		}
 	}),
 	Particle: new Class({
 		

fe7a3948b317c8f052b0cc66f8f086a2034a61f8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-07-17 21:12:42 +0100

    Added the basic framework

---
+++ Physics.js
@@ -0,0 +1,13 @@
+/**
+ * @preserve Physics MooTools plugin v1.0.0
+ * 
+ * Oliver Caldwell 2011 (olivercaldwell.co.uk)
+ */
+var Physics = {
+	World: new Class({
+		
+	}),
+	Particle: new Class({
+		
+	})
+}

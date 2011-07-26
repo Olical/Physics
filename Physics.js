e96a12971df2354a34a98f7062c4a1203191a80a
Oliver Caldwell <oliver@flowdev.co.uk>
2011-07-26 21:24:39 +0100

    Added jsDoc to EVERYTHING

--- Physics.js
+++ Physics.js
@@ -26,7 +26,7 @@ var Physics = {
 			/**
 			 * Resets the particles and positions array
 			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.clear = function() {
 				// Initialise variables
@@ -51,7 +51,7 @@ var Physics = {
 			 * Starts the timed loop that calls each step
 			 * Fires the start event when it starts and the alreadyRunning event if its, well, already running
 			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.start = function() {
 				if(this.interval === false) {
@@ -72,7 +72,7 @@ var Physics = {
 			 * Stops the interval that calls step
 			 * Fires the stop event. It will fire the notRunning event if it is not running yet
 			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.stop = function() {
 				// Only do this if we have an interval
@@ -92,9 +92,11 @@ var Physics = {
 			}.bind(this);
 			
 			/**
+			 * Moves a particle to the specified location, no questions asked
 			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @param {Object} particle The particle to move
+			 * @param {Object} to The new location object containing the x and y coordinates, I trust this is within the worlds bounds
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.moveParticle = function(particle, to) {
 				// Change the positions object value
@@ -108,9 +110,11 @@ var Physics = {
 			}.bind(this);
 			
 			/**
+			 * Applies friction to the given particle on the given axis
 			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @param {Object} particle The particle to apply friction to
+			 * @param {String} axis The name of the axis to apply friction to, either x or y
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.applyFriction = function(particle, axis) {
 				// Apply friction
@@ -127,9 +131,13 @@ var Physics = {
 			}.bind(this);
 			
 			/**
+			 * Moves a particle to a specified location and performs collisions with other particles and walls
+			 * Fires the collision event when it hits another particle, passes the current particle and the one it hit in the arguments
+			 * Also fires the wallCollision event, when it hits a wall. It passes the particle in the arguments
 			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @param {Object} particle The particle to move
+			 * @param {Object} to The new location object containing the x and y coordinates
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.slideParticle = function(particle, to) {
 				// Iniailise variables
@@ -222,9 +230,12 @@ var Physics = {
 			}.bind(this);
 			
 			/**
+			 * Performs a step, called by the interval initialised in the start method
+			 * Applies velocity equivilent to the weight either up or down depending on it being a gas or not
+			 * Then applies friction and then slides to the new location
+			 * Fires the step event and passes the particle array along with the event
 			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.step = function() {
 				// Loop over the particles
@@ -258,9 +269,15 @@ var Physics = {
 			}.bind(this);
 			
 			/**
+			 * Adds a particle to the world
+			 * Fires the addParticle event and passes the new particle
+			 * If there is a particle already there, it does not add it and fires the inUse event
+			 * The inUse event passes the particle you tried to add and the particle that is in the way
+			 * You can overide this by setting the force argument to true
 			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * @param {Object} particle The new particle to add
+			 * @param {Boolean} force If this is true then it will overwrite any existing particles at that point
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.addParticle = function(particle, force) {
 				// Make sure the point is empty
@@ -284,9 +301,12 @@ var Physics = {
 			}.bind(this);
 			
 			/**
-			 * 
-			 * 
-			 * @return {Object} The instance of Physics.World to allow chaining
+			 * Removes the specified particle from the world
+			 * Fires the removeParticle event and passes the removed particle
+			 * If no such particle exists, it fires the noParticle event and again, passes the particle in question
+			 *
+			 * @param {Object} particle The particle to remove
+			 * @returns {Object} The instance of Physics.World to allow chaining
 			 */
 			this.removeParticle = function(particle) {
 				// Get the index of the paticle in the particles array
